c43b9d9d1f3c48f049bd1181f1a2fc215be8089e
mnival <1595998+mnival@users.noreply.github.com>
2021-03-20 09:00:42 +0100

    Add control before launch deploy_config in storage and client

--- client/docker-entrypoint.sh
+++ client/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/usr/lib/bareos/scripts/bareos-config deploy_config "/usr/lib/bareos/defaultconfigs" "/etc/bareos" "bareos-fd"
+if [ ! -d /etc/bareos/bareos-fd.d ]; then
+	/usr/lib/bareos/scripts/bareos-config deploy_config "/usr/lib/bareos/defaultconfigs" "/etc/bareos" "bareos-fd"
+fi
 
 chown bareos:bareos /var/lib/bareos
 
--- storage/docker-entrypoint.sh
+++ storage/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/usr/lib/bareos/scripts/bareos-config deploy_config "/usr/lib/bareos/defaultconfigs" "/etc/bareos" "bareos-sd"
+if [ ! -d /etc/bareos/bareos-sd.d ]; then
+	/usr/lib/bareos/scripts/bareos-config deploy_config "/usr/lib/bareos/defaultconfigs" "/etc/bareos" "bareos-sd"
+fi
 
 [ ! -d /var/lib/bareos/storage ] && mkdir -p /var/lib/bareos/storage
 chown bareos:bareos /var/lib/bareos/ /var/lib/bareos/storage/
